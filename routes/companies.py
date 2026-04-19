7242b600705accf8ccc674211ae921fcbef1dfe6
jdelsaltod <jdelsaltod@student.universidadunie.com>
2026-04-19 16:41:45 +0200

    Corrección de inyección SQL en inserción de comentarios mediante consultas parametrizadas (CWE-89)

--- routes/companies.py
+++ routes/companies.py
@@ -70,7 +70,7 @@ def company_detail(company_id):
     if request.method == 'POST':
         comment = request.form['comment']
         user = session.get('username')
-        conn.execute("INSERT INTO comments (company_id, user, comment) VALUES ("+str(company_id)+", '"+user+"', '"+comment+"')")
+        conn.execute("INSERT INTO comments (company_id, user, comment) VALUES (?, ?, ?)", (company_id, user, comment)).
         conn.commit()
         conn.close()
         flash("Comment added successfully.", "success")
